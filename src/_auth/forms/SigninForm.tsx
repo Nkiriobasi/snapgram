b7c78092ee7d18f13e16021034c89d0c195532c5
Nkiriobasi <obasinkiri@gmail.com>
2023-12-01 16:04:39 +0100

    removed logo svg

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -54,7 +54,6 @@ const SigninForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo.svg" alt="logo" />
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
           Log in to your account
